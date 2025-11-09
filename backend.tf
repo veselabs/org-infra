4eec94857188b5d6bdf613301ae33d6b6db7865a
Veselin Ivanov <me@veselyn.com>
2025-11-09 09:55:42 +0100

    feat: change backend key to organization

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region       = "eu-north-1"
     bucket       = "veselabs-terraform-state"
-    key          = "github/terraform.tfstate"
+    key          = "organization/terraform.tfstate"
     use_lockfile = true
   }
 }
