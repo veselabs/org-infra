bbe00afa793e0115763eb925e2a1956d3b9bf0c2
Veselin Ivanov <me@veselyn.com>
2026-03-04 16:53:43 +0100

    feat: update flake lock file

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "org-infra";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-parts.url = "github:hercules-ci/flake-parts/main";
     treefmt-nix.url = "github:numtide/treefmt-nix/main";
-    devenv.url = "github:cachix/devenv/v1.9";
+    devenv.url = "github:cachix/devenv/v2.0";
   };
 
   outputs = inputs @ {flake-parts, ...}:
