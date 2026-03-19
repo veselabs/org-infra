651e5daeea20fb8761cebd21f7241e2a4ede81be
Veselin Ivanov <me@veselyn.com>
2026-03-19 13:49:28 +0100

    build: enable terraform-validate git hook (#22)

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-parts.url = "github:hercules-ci/flake-parts/main";
-    devenv.url = "github:cachix/devenv/v2.0";
+    devenv.url = "github:cachix/devenv/v2.0.5";
     treefmt-nix.url = "github:numtide/treefmt-nix/main";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    git-hooks.url = "github:cachix/git-hooks.nix/master";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {flake-parts, ...}:
@@ -39,6 +41,7 @@
               languages = {
                 nix.enable = true;
                 terraform.enable = true;
+                terraform.package = pkgs.terraform;
               };
 
               packages = builtins.attrValues {
@@ -74,6 +77,8 @@
                   files = "\\.tf$";
                   pass_filenames = false;
                 };
+                terraform-validate.enable = true;
+                terraform-validate.package = pkgs.terraform;
                 tflint.enable = true;
                 treefmt.enable = true;
                 trim-trailing-whitespace.enable = true;
